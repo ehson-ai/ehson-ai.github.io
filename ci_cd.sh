bb26a31f628a8f9922f148195be8e9e5d648aed7
qgenbio.com <soneunho4502@gmail.com>
2025-02-27 14:44:05 +0900

    Delete ci_cd.sh

--- ci_cd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python3 -m pytest test_math.py
-if [ $? -eq 0 ]; then
-    echo "Tests passed. Deploying..."
-    # 여기에 배포 명령어 추가
-else
-    echo "Tests failed. Deployment aborted."
-fi
